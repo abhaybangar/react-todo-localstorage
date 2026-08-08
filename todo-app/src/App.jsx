21c46cad837ecd9c9decded5e80430986c15776b
Abhay Bangar <abhayb@op.iitg.ac.in>
2026-08-08 12:47:43 +0530

    update the ui and added the completed task checkbox

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,49 +1,84 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
- const [todos, setTodos] = useState([])
- const [input, setInput] = useState('')
- const handleInputChange = (e) => {
-  setInput(e.target.value)
- }
- const addTodo =  () => {
-   if (input.trim() === '') return
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState('')
 
-   setTodos([
-    ...todos,
-    {
-      id: Date.now(),
-      text : input
-    }
-   ])
-   setInput('')
- }
- 
+  const handleInputChange = (e) => {
+    setInput(e.target.value)
+  }
 
+  const addTodo = () => {
+    if (input.trim() === '') return
 
+    setTodos([
+      ...todos,
+      {
+        id: Date.now(),
+        text: input,
+        completed: false
+      }
+    ])
+
+    setInput('')
+  }
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id)
+
+    setTodos(newTodos)
+  }
+
+  const completeTodo = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      }
+
+      return todo
+    })
+
+    setTodos(newTodos)
+  }
 
   return (
     <>
-    <div>
       <h1>Todos</h1>
-      <input type="text" placeholder='Add a todo' value = {input}
-      onChange = {handleInputChange} />
-      <button onClick= {addTodo}>Add</button>
-      <p>Track your tasks here.
-        total todos: {todos.length}
-      </p>
+
+      <input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Enter your todo"
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      <p>Track your tasks here.</p>
+
+      <p>Total todos: {todos.length}</p>
+
       <ul>
-        {todos.map((todo) =>
-        (
-          <li key={todo.id}> {todo.text} </li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => completeTodo(todo.id)}
+            />
+
+            <button onClick={() => deleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
-    </div>
-
     </>
   )
 }
