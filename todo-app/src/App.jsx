0ba7f44798a53867c5655a5d5b525bf98d2e0e36
Abhay Bangar <abhayb@op.iitg.ac.in>
2026-08-08 11:38:41 +0530

    added and render todos

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -5,10 +5,45 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [todos, setTodos] = useState([])
+ const [input, setInput] = useState('')
+ const handleInputChange = (e) => {
+  setInput(e.target.value)
+ }
+ const addTodo =  () => {
+   if (input.trim() === '') return
+
+   setTodos([
+    ...todos,
+    {
+      id: Date.now(),
+      text : input
+    }
+   ])
+   setInput('')
+ }
+ 
+
+
 
   return (
     <>
+    <div>
+      <h1>Todos</h1>
+      <input type="text" placeholder='Add a todo' value = {input}
+      onChange = {handleInputChange} />
+      <button onClick= {addTodo}>Add</button>
+      <p>Track your tasks here.
+        total todos: {todos.length}
+      </p>
+      <ul>
+        {todos.map((todo) =>
+        (
+          <li key={todo.id}> {todo.text} </li>
+        ))}
+      </ul>
+    </div>
+
     </>
   )
 }
